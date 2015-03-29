f0cd0464965e052ed09b4414d8c3b305ed42a30d
gatita <hatamyarg@gmail.com>
2015-03-28 19:25:12 -0700

    library initial commit

---
+++ challenge1/library.py
@@ -0,0 +1,46 @@
+class Library(object):
+    def __init__(self):
+        self.shelves = []
+    
+    def add_shelf(self, shelf):
+        self.shelves.append(shelf)
+        
+    def number_of_shelves(self):
+        return len(self.shelves)
+           
+        
+class Shelf(object):
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def remove_book(self, book):
+        self.books.remove(book)
+        
+    def show_books(self):
+        for book in self.books:
+            print book.title + ' by ' + book.author
+
+class Book(object):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        
+    def enshelf(self, shelf):
+        shelf.add_book(self)
+        
+    def unshelf(self, shelf):
+        shelf.remove_book(self)
+        
+library = Library()
+nonfiction = Shelf()
+book1 = Book('Ideas and Opinions', 'Albert Einstein')
+book2 = Book('Speak, Memory', 'Vladimir Nabokov')
+book3 = Book('The Elements of Style', 'William Strunk and E.B. White')
+
+
+    
+
+        

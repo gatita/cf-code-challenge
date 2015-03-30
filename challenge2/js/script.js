6960b1c42669ea6b72d49cd7b2778a2dbd061b95
gatita <hatamyarg@gmail.com>
2015-03-29 19:10:44 -0700

    remove table rows on click

--- challenge2/js/script.js
+++ challenge2/js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
     $('#add').click(function(){
         var newTodo = $('#new').val();
-        $('<tr><td>' + newTodo + '</td></tr>').prependTo('#todos > tbody');
+        $("<tr><td>" + newTodo + '</td></tr>').prependTo('#todos > tbody');
         $('#new').val('');
+        $('#todos td').click(function(){
+            $(this).remove();
+        })
     });
+
 });
+
 

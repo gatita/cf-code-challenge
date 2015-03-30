90d9bbe65a328ca53cf133890cab16ebe57d2564
gatita <hatamyarg@gmail.com>
2015-03-29 19:43:20 -0700

    blank form will not submit, and enclosed addrow in a function

--- challenge2/js/script.js
+++ challenge2/js/script.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
     $('#add').click(function(){
+        addRow();
+    });
+    $('#new').keydown(function(e) {
+        if (e.keyCode == 13) {
+            addRow();
+        }
+    });
+});
+
+function addRow() {
+    if ($('#new').val() != '') {
         var newTodo = $('#new').val();
         $("<tr><td>" + newTodo + '</td></tr>').prependTo('#todos > tbody');
         $('#new').val('');
         $('#todos td').click(function(){
             $(this).remove();
-        })
-    });
-
-});
+        });
+    }
+};
 
 

ad59e0ebcb10b2197806a2a30d838c76e64bb803
gatita <hatamyarg@gmail.com>
2015-03-31 23:22:35 -0700

    added create view and template

--- challenge3/mysite/mysite/urls.py
+++ challenge3/mysite/mysite/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('users.urls')),
+    url(r'^', include('users.urls', namespace="users")),
 )
--- challenge3/mysite/users/views.py
+++ challenge3/mysite/users/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, HttpResponse
+from django.forms import ModelForm
+from django.core.urlresolvers import reverse
 
 from users.models import User
+
+class AddUser(ModelForm):
+    class Meta:
+        model = User
 
 def user_list(request):
     users = User.objects.all()
     return render(request, 'users/user_list.html', {'users': users})
+
+def create(request):
+    form = AddUser(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect(reverse('users:user_list'))
+    return render(request, 'users/create.html', {'form': form})
+

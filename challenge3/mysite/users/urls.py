d6b2741e53a47049854506b30d08928cae254611
gatita <hatamyarg@gmail.com>
2015-03-31 22:55:55 -0700

    added user_list view and template

--- challenge3/mysite/users/urls.py
+++ challenge3/mysite/users/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from users import views
 
 urlpatterns = patterns('',
-    url(r'^create/$', views.create, name='create'),
-    url(r'^(?P<user_id>\d+)/edit/$', views.edit, name='edit'),
-    url(r'^(?p<user_id>\d+)/delete/$', views.delete, name='delete'),
+    # url(r'^create/$', views.create, name='create'),
+    # url(r'^(?P<user_id>\d+)/edit/$', views.edit, name='edit'),
+    # url(r'^(?p<user_id>\d+)/delete/$', views.delete, name='delete'),
     url(r'^$', views.user_list, name='user_list'),
     )
--- challenge3/mysite/users/views.py
+++ challenge3/mysite/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
 
-# Create your views here.
+from users.models import User
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'users/user_list.html', {'users': users})

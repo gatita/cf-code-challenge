3e3fc09c2603840435bdae0411a0eb25ac53ebc0
gatita <hatamyarg@gmail.com>
2015-03-31 21:44:47 -0700

    added User model to models

--- challenge3/mysite/users/models.py
+++ challenge3/mysite/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40, blank=True)
+    email = models.EmailField()
+
+    def __unicode__(self):
+        return self.first_name + ' ' + self.last_name

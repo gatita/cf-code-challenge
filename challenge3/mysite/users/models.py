4777d82f24c8882506cac4c4364406fafc1521bc
gatita <hatamyarg@gmail.com>
2015-04-02 21:30:17 -0700

    email is now not required

--- challenge3/mysite/users/models.py
+++ challenge3/mysite/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField('First Name', max_length=40)
     last_name = models.CharField('Last Name', max_length=40, blank=True)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
 
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name
